312c86fac85b57ab3ba95b8470f6233c42524388
vishalryyadav <vishal.ry.yadav@gmail.com>
2026-09-11 19:19:46 +0530

    Wire project scanner into application shell

--- artifacts/ecodev/src/App.tsx
+++ artifacts/ecodev/src/App.tsx
@@ -1,11 +1,13 @@
 import AppReal from './AppReal';
 import DeviceWidget from './DeviceWidget';
+import ProjectScanner from './ProjectScanner';
 
 export default function App() {
   return (
     <>
       <AppReal />
       <DeviceWidget />
+      <ProjectScanner />
     </>
   );
 }
